b4d61e4c36dcf43381a15dab126ac54b6e169a23
Roman Krejcik <farin@farin.cz>
2021-04-17 09:53:04 +0200

    fix: wrong meeple type can be user when moving outside CoC

--- src/renderer/components/game/actions/items/MeeplePlacementItem.vue
+++ src/renderer/components/game/actions/items/MeeplePlacementItem.vue
@@ -72,7 +72,7 @@ export default {
         let meepleId
         if (this.coc) {
           const quarter = FEATURE_QUARTERS[this.featureOn(opt).type]
-          meepleId = this.$store.state.game.deployedMeeples.find(m => m.player === this.player && m.location === quarter).id
+          meepleId = this.$store.state.game.deployedMeeples.find(m => m.player === this.player && m.location === quarter && m.type === this.meeple).id
         } else {
           meepleId = this.meepleIdFromSupply(this.player, this.meeple)
         }
