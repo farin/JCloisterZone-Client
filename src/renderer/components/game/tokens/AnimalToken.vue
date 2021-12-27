9c84a7ef3b834116f86e8957bbe10ca56d24d499
Roman Krejcik <farin@farin.cz>
2021-12-27 10:22:05 +0100

    event history can show Animal token (bigtop)

---
+++ src/renderer/components/game/tokens/AnimalToken.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg viewBox="-1 -1 2 2">
+    <circle r="1" fill="#ba9745" />
+    <text x="-0.45" y="0.6" fill="white" style="font-size: 1.7px; font-weight: 600">{{ points }}</text>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    points: { type: Number, required: true }
+  }
+}
+</script>
