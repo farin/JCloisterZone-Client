535524293d868ffa3ba550a97b0164aee4866f8a
Roman Krejcik <farin@farin.cz>
2022-01-31 22:28:42 +0100

    cleanup tunnel fallback

--- src/renderer/plugins/theme.js
+++ src/renderer/plugins/theme.js
@@ -122,12 +122,12 @@ class Theme extends EventsBase {
     let feature = tile?.features[`${featureType}/${loc}`]
 
     if (!feature) {
-	  if (featureType == "Tunnel") {
-		// Failback for old Addons
-		const tunnelX = (loc == 'N' || loc == 'S') ? 500 : (loc == 'W' ? 250 : 750);
-		const tunnelY = (loc == 'W' || loc == 'E') ? 500 : (loc == 'N' ? 250 : 750);
-		return {
-		  clip: {
+      // Failback for old Addons - TODO remove in future
+      if (featureType === "Tunnel") {
+        const tunnelX = (loc == 'N' || loc == 'S') ? 450 : (loc == 'W' ? 225 : 675);
+        const tunnelY = (loc == 'W' || loc == 'E') ? 450 : (loc == 'N' ? 225 : 675);
+        return {
+          clip: {
             shape: "circle",
             cx: tunnelX,
             cy: tunnelY,
@@ -138,7 +138,7 @@ class Theme extends EventsBase {
             tunnelY
           ],
           rotation: rotation
-	    }
+        }
       }
       throw new Error(`Artwork doesn't provide feature for ${id} ${featureType}/${loc}`)
     }
