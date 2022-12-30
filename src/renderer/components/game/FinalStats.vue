22cccd90568922f2d820308a54e63b5643e47c94
stanolacko <stano.lacko@gmail.com>
2022-12-30 09:51:15 +0100

    Fixing game.features i18n

--- src/renderer/components/game/FinalStats.vue
+++ src/renderer/components/game/FinalStats.vue
@@ -42,54 +42,54 @@
         {{ val }}
       </div>
 
-      <div class="header roads" :title="$t('feature.roads')"><StandaloneTileImage tile-id="BA/RFr" :size="40" /></div>
+      <div class="header roads" :title="$t('game.feature.roads')"><StandaloneTileImage tile-id="BA/RFr" :size="40" /></div>
       <div v-for="(val, idx) in stats.points.road" :key="'roads-'+idx" class="roads value">
         {{ val }}
       </div>
 
-      <div class="header cities" :title="$t('feature.cities')"><StandaloneTileImage tile-id="BA/Cccc+" :size="40" /></div>
+      <div class="header cities" :title="$t('game.feature.cities')"><StandaloneTileImage tile-id="BA/Cccc+" :size="40" /></div>
       <div v-for="(val, idx) in stats.points.city" :key="'cities-'+idx" class="cities value">
         {{ val }}
       </div>
 
-      <div class="header monasteries" :title="$t('feature.monasteries')"><img src="~/assets/features/C1/cloister.png" height="40"></div>
+      <div class="header monasteries" :title="$t('game.feature.monasteries')"><img src="~/assets/features/C1/cloister.png" height="40"></div>
       <div v-for="(val, idx) in stats.points.monastery" :key="'monasteries-'+idx" class="monasteries value">
         {{ val }}
       </div>
 
-      <div class="header garden" :title="$t('feature.gardens')"><img src="~/assets/features/C1/garden.png" height="40"></div>
+      <div class="header garden" :title="$t('game.feature.gardens')"><img src="~/assets/features/C1/garden.png" height="40"></div>
       <div v-for="(val, idx) in stats.points.garden" :key="'garden-'+idx" class="garden value">
         {{ val }}
       </div>
 
-      <div class="header fields" :title="$t('feature.fields')"><StandaloneTileImage tile-id="GQ/F" :size="40" /></div>
+      <div class="header fields" :title="$t('game.feature.fields')"><StandaloneTileImage tile-id="GQ/F" :size="40" /></div>
       <div v-for="(val, idx) in stats.points.field" :key="'fields-'+idx" class="fields value">
         {{ val }}
       </div>
 
       <template v-if="stats.points['special-monastery'].some(p => p)">
-        <div class="header special-monasteries" :title="$t('feature.special-monasteries')"><StandaloneTileImage tile-id="MO/M1" :size="40" /></div>
+        <div class="header special-monasteries" :title="$t('game.feature.special-monasteries')"><StandaloneTileImage tile-id="MO/M1" :size="40" /></div>
         <div v-for="(val, idx) in stats.points['special-monastery']" :key="'special-monasteries-'+idx" class="special-monasteries value">
           {{ val }}
         </div>
       </template>
 
       <template v-if="stats.points.castle.some(p => p)">
-        <div class="header castle" :title="$t('feature.castles')"><img src="~/assets/figures/castle.png" height="40"></div>
+        <div class="header castle" :title="$t('game.feature.castles')"><img src="~/assets/figures/castle.png" height="40"></div>
         <div v-for="(val, idx) in stats.points.castle" :key="'castles-'+idx" class="castles value">
           {{ val }}
         </div>
       </template>
 
       <template v-if="stats.points.watchtower.some(p => p)">
-        <div class="header watchtowers" :title="$t('feature.watchtowers')"><StandaloneTileImage tile-id="WT/CFff_3C" :size="40" /></div>
+        <div class="header watchtowers" :title="$t('game.feature.watchtowers')"><StandaloneTileImage tile-id="WT/CFff_3C" :size="40" /></div>
         <div v-for="(val, idx) in stats.points.watchtower" :key="'watchtowers-'+idx" class="watchtowers value">
           {{ val }}
         </div>
       </template>
 
-      <!-- <template v-if="stats.points['trade-goods'].some(p => p)">
-        <div class="header traders" :title="$t('feature.trade-goods')"><img src="~/assets/figures/trade.png" height="20"></div>
+      <template v-if="stats.points['trade-goods'].some(p => p)">
+        <div class="header traders" :title="$t('game.feature.trade-goods')"><img src="~/assets/figures/trade.png" height="20"></div>
         <div v-for="(val, idx) in stats.points['trade-goods']" :key="'traders-'+idx" class="traders value">
           {{ val }}
         </div>
@@ -110,11 +110,19 @@
       </template>
 
       <template v-if="stats.points.gold.some(p => p)">
-        <div class="header gold" :title="$t('features.gold')"><img src="~/assets/figures/gold.png" height="20"></div>
+        <div class="header gold" :title="$t('game.feature.gold')"><img src="~/assets/figures/gold.png" height="20"></div>
         <div v-for="(val, idx) in stats.points.gold" :key="'gold-'+idx" class="gold value">
           {{ val }}
         </div>
-      </template> -->
+      </template>
+
+      <template v-if="stats.points.fairy.some(p => p)">
+        <div class="header fairy" :title="$t('game.feature.fairy')"><NeutralFigure figure="fairy" :width="40" :height="40" /></div>
+        <div v-for="(val, idx) in stats.points.fairy" :key="'fairy-'+idx" class="fairy value">
+          {{ val }}
+        </div>
+      </template>
+
     </div>
   </div>
 </template>
@@ -126,11 +134,13 @@ import flatten from 'lodash/flatten'
 import debounce from 'lodash/debounce'
 
 import Meeple from '@/components/game/Meeple'
+import NeutralFigure from '@/components/game/NeutralFigure'
 import StandaloneTileImage from '@/components/game/StandaloneTileImage'
 
 export default {
   components: {
     Meeple,
+    NeutralFigure,
     StandaloneTileImage
   },
 
@@ -166,11 +176,12 @@ export default {
           'field': (new Array(this.players.length)).fill(0),
           'special-monastery': (new Array(this.players.length)).fill(0),
           'castle': (new Array(this.players.length)).fill(0),
-          'watchtower': (new Array(this.players.length)).fill(0)
-          // 'trade-goods': (new Array(this.players.length)).fill(0),
-          // 'robber': (new Array(this.players.length)).fill(0),
-          // 'king': (new Array(this.players.length)).fill(0),
-          // 'gold': (new Array(this.players.length)).fill(0)
+          'watchtower': (new Array(this.players.length)).fill(0),
+          'trade-goods': (new Array(this.players.length)).fill(0),
+          'robber': (new Array(this.players.length)).fill(0),
+          'king': (new Array(this.players.length)).fill(0),
+          'gold': (new Array(this.players.length)).fill(0),
+          'fairy': (new Array(this.players.length)).fill(0)
         }
       }
       this.history.forEach(h => {
