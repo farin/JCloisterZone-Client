b1dab514048aadc1d43da370adc0d9ee78afe78a
stanolacko <stano.lacko@gmail.com>
2023-01-29 20:04:07 +0100

    Locale fixes plus pluralization definitions

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -38,6 +38,62 @@ module.exports = {
   },
 
   i18n: {
+    pluralizationRules: {
+      /**
+       * @param choice {number} a choice index given by the input to $tc: `$tc('path.to.rule', choiceIndex)`
+       * @param choicesLength {number} an overall amount of available choices
+       * @returns a final choice index to select plural word by
+       */
+      'ru': function(choice, choicesLength) {
+        // this === VueI18n instance, so the locale property also exists here
+
+        if (choice === 0) {
+          return 0;
+        }
+
+        const teen = choice > 10 && choice < 20;
+        const endsWithOne = choice % 10 === 1;
+
+        if (choicesLength < 4) {
+          return (!teen && endsWithOne) ? 1 : 2;
+        }
+        if (!teen && endsWithOne) {
+          return 1;
+        }
+        if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+          return 2;
+        }
+
+        return (choicesLength < 4) ? 2 : 3;
+      },
+      'sk': function(choice, choicesLength) {
+        console.log(choice, choicesLength);
+        if (choice === 0) {
+          return 3;
+        }
+        if (choice === 1) {
+          return 1;
+        }
+        if (choice>1 && choice<5) {
+          return 2;
+        }
+        // 5 and more or less then 0
+        return 3;
+      },
+      'cs': function(choice, choicesLength) {
+        if (choice === 0) {
+          return 3;
+        }
+        if (choice === 1) {
+          return 1;
+        }
+        if (choice>1 && choice<5) {
+          return 2;
+        }
+        // 5 and more or less then 0
+        return 3;
+      }
+    },
     strategy: 'no_prefix',
     detectBrowserLanguage: false,
     locales: [
