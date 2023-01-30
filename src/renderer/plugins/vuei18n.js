701055ba6aef63012abea47ec308043b36a77397
stanolacko <stano.lacko@gmail.com>
2023-01-30 06:52:04 +0100

    Pluralization rules for all supported languages or expecting languages

--- src/renderer/plugins/vuei18n.js
+++ src/renderer/plugins/vuei18n.js
@@ -1,43 +1,44 @@
-function locale_cs_sk(choice, choicesLength) {
-  if (choice === 0) {
-    return 0;
-  }
-  if (choice === 1) {
-    return 1;
-  }
-  if (choice > 1 && choice <= 4) {
-    return 2;
-  }
-  return 3;
+// http://docs.translatehouse.org/projects/localization-guide/en/latest/l10n/pluralforms.html
+function locale_cs_sk(n, choicesLength) {
+  const plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;
+  return (plural+1)
+}
+function locale_la(n, choicesLength) {
+  const plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);
+  return (plural+1)
+}
+function locale_lt(n, choicesLength) {
+  const plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && (n%100<10 || n%100>=20) ? 1 : 2);
+  return (plural+1)
+}
+function locale_pl(n, choicesLength) {
+  const plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
+  return (plural+1)
+}
+function locale_ro(choice, choicesLength) {
+  const plural=(n==1 ? 0 : (n==0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2);
+  return (plural+1);
 }
 function locale_ru(choice, choicesLength) {
-  if (choice === 0) {
-    return 0;
-  }
-
-  const teen = choice > 10 && choice < 20;
-  const endsWithOne = choice % 10 === 1;
-
-  if (choicesLength < 4) {
-    return (!teen && endsWithOne) ? 1 : 2;
-  }
-  if (!teen && endsWithOne) {
-    return 1;
-  }
-  if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
-    return 2;
-  }
-
-  return (choicesLength < 4) ? 2 : 3;
+  const plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
+  return (plural+1);
+}
+function locale_uk(choice, choicesLength) {
+  const plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);
+  return (plural+1);
 }
       
 export default (context) => {
   return {
-    fallbackLocale: "cs",
     pluralizationRules: {
       "cs": locale_cs_sk,
+      "la": locale_la,
+      "lt": locale_lt,
+      "pl": locale_pl,
+      "ro": locale_ro,
       "ru": locale_ru,
-      "sk": locale_cs_sk
+      "sk": locale_cs_sk,
+      "uk": locale_uk,
     }
   }
 }
