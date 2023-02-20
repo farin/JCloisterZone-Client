ea957cccc0a9ae0c14f370ee487e50ea74a384c8
Roman Krejcik <farin@farin.cz>
2023-02-20 23:05:20 +0100

    hint meta keys

--- src/renderer/plugins/i18n.js
+++ src/renderer/plugins/i18n.js
@@ -1,5 +1,10 @@
 import { ipcRenderer } from 'electron'
 
+// hint meta keys as used to extractor
+// $t('@author')
+// $t('@jcz-version')
+// $t('@version')
+
 export default function ({ app }) {
   app.i18n.onLanguageSwitched = (oldLocale, newLocale) => {
     const messages = app.i18n.getLocaleMessage(newLocale)
